49d04e2fb17886fc6136043d39175c8bc689a232
Justin Stewart <Justinstewart3313@gmail.com>
2018-12-02 22:14:15 -0700

    get palettes to back end, clean up front end

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -68,32 +68,81 @@ const deleteProject = event => {
 };
 
 const savePalette = () => {
-  $(".list-name").append(`
-  <div class='project-swatches'>
-    <div 
-      class='swatch1' 
-      style="background-color:${$(".a").css("background-color")};">
-    </div>
-    <div 
-      class='swatch2' 
-      style="background-color:${$(".b").css("background-color")};">
-    </div>
-    <div 
-      class='swatch3' 
-      style="background-color:${$(".c").css("background-color")};">
-    </div>
-    <div 
-      class='swatch4' 
-      style="background-color:${$(".d").css("background-color")};">
-    </div>
-    <div 
-      class='swatch5' 
-      style="background-color:${$(".e").css("background-color")};">
-    </div><button class='trash-btn'></button>
-  </div>`);
+  event.preventDefault();
+  let project_id = $("#project-select option:selected").attr("data-id");
+  let name = $(".palette-name-input").val();
+  let hex1 = $(".a").css("background-color");
+  let hex2 = $(".b").css("background-color");
+  let hex3 = $(".c").css("background-color");
+  let hex4 = $(".d").css("background-color");
+  let hex5 = $(".e").css("background-color");
+
+  postPaletteToDB(project_id, name, hex1, hex2, hex3, hex4, hex5);
+
+  let selectedProject = $("#project-select option:selected").attr("data-id");
+  let savedProject = $(".list-name").attr("data-id");
+  let paletteName = $(".palette-name-input").val();
+  if (savedProject === selectedProject) {
+    $(".list-name").append(`
+    <div class='project-swatches'>
+    <p class='swatch-name'>${paletteName}</p>
+      <div 
+        class='swatch1' 
+        style="background-color:${$(".a").css("background-color")};">
+      </div>
+      <div 
+        class='swatch2' 
+        style="background-color:${$(".b").css("background-color")};">
+      </div>
+      <div 
+        class='swatch3' 
+        style="background-color:${$(".c").css("background-color")};">
+      </div>
+      <div 
+        class='swatch4' 
+        style="background-color:${$(".d").css("background-color")};">
+      </div>
+      <div 
+        class='swatch5' 
+        style="background-color:${$(".e").css("background-color")};">
+      </div><button class='trash-btn'></button>
+    </div>`);
+  }
 };
 
-
+const postPaletteToDB = async (
+  projectId,
+  paletteName,
+  hex1,
+  hex2,
+  hex3,
+  hex4,
+  hex5
+) => {
+  try {
+    const response = fetch(`api/v1/palettes`, {
+      method: "POST",
+      mode: "cors",
+      credentials: "same-origin",
+      body: JSON.stringify({
+        name: paletteName,
+        hex1,
+        hex2,
+        hex3,
+        hex4,
+        hex5,
+        project_id: projectId
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    });
+    const data = response.json();
+    console.log(data);
+  } catch (e) {
+    throw Error("another thing went wrong");
+  }
+};
 
 $(".save-palette").click(() => {
   savePalette();
@@ -118,14 +167,16 @@ const saveProject = async id => {
     return acc;
   }, {});
 
-  let projectName = `<li class='list-name' id='${
+  let projectName = `<li class='list-name' data-id='${
     projectData.id
   }'><p class='project-name'> ${projectData.title} </p></li>`;
   $(".projects").append(projectName);
 
   const projectTitle = $(".project-name-input").val();
   $("#project-select").append(
-    `<option value="" id='${projectData.id}'>${projectData.title}</option>`
+    `<option value="${projectData.title}" data-id='${projectData.id}'>${
+      projectData.title
+    }</option>`
   );
 };
 
